9d2e766c4557dbfbe5238b7227236877128918e4
mgmetehan <mgmetehan@hotmail.com>
2023-06-29 20:09:58 +0300

    init project

---
+++ src/test/java/com/mgmetehan/bankaccount/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
